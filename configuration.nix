7b37e5e21315e6ab6a891167f050ca040742192f
jhparker77 <github@jhparker.uk>
2025-03-03 19:50:15 +0000

    adding hyprland

--- configuration.nix
+++ configuration.nix
@@ -144,7 +144,12 @@
     git # git version control
   ];
 
-
+  programs.hyprland = {
+    # Install the packages from nixpkgs
+    enable = true;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+  };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
